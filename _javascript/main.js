fbd8aaf8dbb07eabb47bb49aae8bd8eaf1393534
Lilian Kempees <45563232+YolaLilian@users.noreply.github.com>
2021-03-29 02:01:03 +0200

    Major content and styling update pt. 2

--- _javascript/main.js
+++ _javascript/main.js
@@ -25,5 +25,60 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const variable1 = document.getElementById("crSes1-1");
+  variable1.addEventListener("click", function(e) {
+    const element = this.closest('div')
+    const modalElement = element.previousElementSibling;
+    modalElement.classList.add("is-active");
+  });
+
+  const variable2 = document.getElementById("crSes1-2");
+  variable2.addEventListener("click", function(e) {
+    const element = this.closest('div')
+    const modalElement = element.previousElementSibling;
+    modalElement.classList.add("is-active");
+  });
+
+  const variable3 = document.getElementById("crSes2-1");
+  variable3.addEventListener("click", function(e) {
+    const element = this.closest('div')
+    const modalElement = element.previousElementSibling;
+    modalElement.classList.add("is-active");
+  });
+  
+  const variable4 = document.getElementById("crSes2-2");
+  variable4.addEventListener("click", function(e) {
+    const element = this.closest('div')
+    const modalElement = element.previousElementSibling;
+    modalElement.classList.add("is-active");
+  });
+  
+  const closeButton1 = document.getElementById("modal-close-1");
+  closeButton1.addEventListener("click", function(e) {
+    const element = this.closest('div')
+    element.classList.remove("is-active");
+  });
+
+  const closeButton2 = document.getElementById("modal-close-2");
+  closeButton2.addEventListener("click", function(e) {
+    const element = this.closest('div')
+    element.classList.remove("is-active");
+  });
+
+  const closeButton3 = document.getElementById("modal-close-3");
+  closeButton3.addEventListener("click", function(e) {
+    const element = this.closest('div')
+    element.classList.remove("is-active");
+  });
+
+  const closeButton4 = document.getElementById("modal-close-4");
+  closeButton4.addEventListener("click", function(e) {
+    const element = this.closest('div')
+    element.classList.remove("is-active");
+  });
 
 });
