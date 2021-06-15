f9d9853a0041ed0d4a2f2f23975160a15f719a18
Lennart Kimmel <43109871+lennartkimmel@users.noreply.github.com>
2021-06-15 12:56:37 +0200

    Added the unstyled mood popup on profile + implemented user icon and changed pathing accordingly.

--- Ionic App/Ionic/BodyBuddy/src/App.tsx
+++ Ionic App/Ionic/BodyBuddy/src/App.tsx
@@ -7,6 +7,7 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs,
+  IonImg,
   IonFab, 
   IonFabButton, 
   IonFabList,
@@ -18,13 +19,15 @@ import { ProtectedRoute } from './components/ProtectedRoute';
 
 import Recipes from './pages/Recipes'
 import Workouts from './pages/Workouts';
-import User from './pages/User';
+import User from './pages/Profile';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Mood from './pages/Mood';
 import Coach from './pages/Coach';
 
 import UIContext from "./MyContext";
+
+import './App.css'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -83,11 +86,12 @@ export default function App() {
 						<IonTabButton tab="mood" href="/mood">
 							<IonIcon icon={home} />
 						</IonTabButton>
-						<IonTabButton tab="user" href="/user">
+						<IonTabButton tab="coach" href="/coach">
 							<IonIcon icon={person} />
 						</IonTabButton>
-						<IonTabButton tab="coach" href="/coach">
-							<IonIcon icon={heart} />
+						<IonTabButton tab="user" href="/user">
+							{/* <IonIcon icon={heart} /> */}
+							<IonImg className="menu__picture" src="../../assets/images/background/profile_picture.png" />
 						</IonTabButton>
 					</IonTabBar>
 				</IonTabs>
